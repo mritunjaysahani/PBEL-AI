4892dfaa5bf15d477b96657136d6a7ef7d772422
Mritunjay Sahani <mrityunjayshn@gmail.com>
2026-06-20 04:40:08 +0000

    List Set Tuple Dictionary

---
+++ Day2.py
@@ -0,0 +1,37 @@
+#List
+a=[10,2,3,4,5]
+print(a)
+print(type(a))
+#Methods of list
+print(len(a))
+a.append(6)
+a.insert(0,1)
+print(a)
+a.extend([7,8,9,10])
+print(a)
+a.remove(10)
+a.pop()
+print(a)
+#  properties of list
+print(a[0])
+a[0]=100
+### muatble duplicacy hetroginity ordered
+
+
+
+#tuple
+b=(10,2,3,4,5)
+print(b)
+print(type(b))
+
+
+
+#set 
+c={10,2,3,4,5}
+print(c)
+print(type(c))
+
+#dictionary
+d={"name":"john","age":30,"city":"new york"}
+print(d)
+print(type(d))
